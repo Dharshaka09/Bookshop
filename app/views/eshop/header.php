f232207e9df8a37b5bb27bc0ea6124a508388f9c
Ruvindi97 <hirushiruvindi93@gmail.com>
2021-08-24 16:42:28 +0530

    Header

--- app/views/eshop/header.php
+++ app/views/eshop/header.php
@@ -32,8 +32,8 @@
 					<div class="col-sm-6">
 						<div class="contactinfo">
 							<ul class="nav nav-pills">
-								<li><a href="#"><i class="fa fa-phone"></i> +2 95 01 88 821</a></li>
-								<li><a href="#"><i class="fa fa-envelope"></i> info@domain.com</a></li>
+								<li><a href="#"><i class="fa fa-phone"></i> contact no</a></li>
+								<li><a href="#"><i class="fa fa-envelope"></i> email</a></li>
 							</ul>
 						</div>
 					</div>
@@ -57,38 +57,14 @@
 				<div class="row">
 					<div class="col-sm-4">
 						<div class="logo pull-left">
-							<a href="index"><img src="<?= ASSETS . THEME ?>images/home/logo.png" alt="" /></a>
+							<a href="index"><img src="<?= ASSETS . THEME ?>images/home/seevali_logo.png" alt="" /></a>
 						</div>
-						<div class="btn-group pull-right">
-							<div class="btn-group">
-								<button type="button" class="btn btn-default dropdown-toggle usa" data-toggle="dropdown">
-									USA
-									<span class="caret"></span>
-								</button>
-								<ul class="dropdown-menu">
-									<li><a href="#">Canada</a></li>
-									<li><a href="#">UK</a></li>
-								</ul>
-							</div>
-							
-							<div class="btn-group">
-								<button type="button" class="btn btn-default dropdown-toggle usa" data-toggle="dropdown">
-									DOLLAR
-									<span class="caret"></span>
-								</button>
-								<ul class="dropdown-menu">
-									<li><a href="#">Canadian Dollar</a></li>
-									<li><a href="#">Pound</a></li>
-								</ul>
-							</div>
-						</div>
+						
 					</div>
 					<div class="col-sm-8">
 						<div class="shop-menu pull-right">
 							<ul class="nav navbar-nav">
 								<li><a href="#"><i class="fa fa-user"></i> Account</a></li>
-								<li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
-								<li><a href="checkout"><i class="fa fa-crosshairs"></i> Checkout</a></li>
 								<li><a href="cart"><i class="fa fa-shopping-cart"></i> Cart</a></li>
 								<li><a href="login"><i class="fa fa-lock"></i> Login</a></li>
 							</ul>
@@ -113,22 +89,14 @@
 						<div class="mainmenu pull-left">
 							<ul class="nav navbar-nav collapse navbar-collapse">
 								<li><a href="index" class="active">Home</a></li>
-								<li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
+								<li class="dropdown"><a href="#">Books<i class="fa fa-angle-down"></i></a>
                                     <ul role="menu" class="sub-menu">
-                                        <li><a href="shop">Products</a></li>
-										<li><a href="product-details">Product Details</a></li> 
-										<li><a href="checkout">Checkout</a></li> 
-										<li><a href="cart">Cart</a></li> 
-										<li><a href="login">Login</a></li> 
+                                        <li><a href="shop">English Books</a></li>
+										<li><a href="product-details">සිංහල පොත්</a>
+									</li> 		
                                     </ul>
                                 </li> 
-								<li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
-                                    <ul role="menu" class="sub-menu">
-                                        <li><a href="blog">Blog List</a></li>
-										<li><a href="blog-single">Blog Single</a></li>
-                                    </ul>
-                                </li> 
-								<li><a href="404">404</a></li>
+								<li><a href="contact-us">About Us</a></li>
 								<li><a href="contact-us">Contact</a></li>
 							</ul>
 						</div>
